204ac95477e5676410a3bcd5a30cb838b3ecd270
KrishnenduDG <krishnendu.kdg@gmail.com>
2025-04-06 23:37:21 +0530

    minor fix

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -45,7 +45,7 @@ const Header = () => {
               <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                 <Avatar className="h-8 w-8">
                   <AvatarImage
-                    src={profileDetails.profileImage}
+                    src={profileDetails.profileImage || currentUser.avatar}
                     alt={currentUser.name}
                   />
                   <AvatarFallback>
